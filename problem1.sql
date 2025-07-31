0e032fd637ded7681b33bf0a06ce4089b1cd292f
SandhyaSambattula <sandhyasambattula@gmail.com>
2025-07-31 10:53:47 +0530

    done

--- problem1.sql
+++ problem1.sql
@@ -5,3 +5,8 @@ CREATE TABLE song (
 INSERT INTO song (title, artist) VALUES
 ('Shape of you', 'Ed Sheeran');
 SELECT * FROM song WHERE LOWER(artist) = LOWER('ed Sheeran');
+
+
+UPDATE site_user
+SET lastname = 'Rush'
+WHERE firstname = 'Alexa'
--- src/main/java/UpdateARecord.java
+++ src/main/java/UpdateARecord.java
@@ -45,4 +45,12 @@ public class UpdateARecord {
             System.out.println("problem1: " + e.getMessage() + '\n');
         }
     }
+    public class Main {
+        public static void main(String[] args) {
+            UpdateARecord obj = new UpdateARecord();
+            obj.problem();
+        }
+    }
 }
+
+
